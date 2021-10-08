0708f60e5864ccca3c4bae490cbe15dda401c035
Alexander Avdacev <docaa@Alexanders-MacBook-Air.local>
2021-10-08 18:07:06 +0300

    Create project

---
+++ Model/EmployeeOffice.swift
@@ -0,0 +1,17 @@
+//
+//  Employee.swift
+//  CompanyN
+//
+//  Created by 111 on 9/13/21.
+//  Copyright © 2021 111. All rights reserved.
+//
+
+import Foundation
+
+
+enum EmployeeOffice: String {
+    case director
+    case manager
+    case officeWorker = "office worker"
+    case accountant
+}
